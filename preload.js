13cca06ed6e5dc6a7eb2bc3d98143ba64c9c49c2
SabarishCodeWizard <ravik60656@gmail.com>
2025-11-04 20:16:20 +0530

    rollback the  Login / Google Sign-In

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    savePDF: (htmlContent, filename) => ipcRenderer.invoke('save-pdf', htmlContent, filename),
-    isElectron: true
-});
